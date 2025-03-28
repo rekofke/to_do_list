6c83cad5bed86bbe15bdc06077adc926637f1da5
Eric Rekofke <rekofke.eric@gmail.com>
2025-03-28 10:44:29 -0700

    Update to_do.py
    fixed the spacing and changed the for/else loop in my view_tasks() to an if/for/else conditional

--- to_do.py
+++ to_do.py
@@ -8,52 +8,54 @@ def initializelist():
 
 # Main Menu Function
 def display_menu():
-    print("To Do List Application")
-    print("1. Add Task")
-    print("2. View All Tasks")
-    print("3. Remove Task")
-    print("4. Exit")
+    print("\nTo Do List Application\n")
+    print("\n1. Add Task\n")
+    print("\n2. View All Tasks\n")
+    print("\n3. Remove Task\n")
+    print("\n4. Exit\n")
 
 # Add Task Function
 def add_task():
-    task = input("What is the task you would like to add? ")
+    task = input("\nWhat is the task you would like to add?\n ")
     tasks.append(task)
-    print("Task {task} has been added successfully!")
+    print("\nTask {task} has been added successfully!\n")
 
 # View All Tasks Function
 def view_tasks():
-    print("All Tasks")
-    for task in tasks:
-        print(task)
-    else:
-        print("No tasks to display")
+    print("\nAll Tasks\n")
+    if tasks:  
+        for task in tasks:
+            print(task)
+    else:  
+        print("\nNo tasks to display\n")
 
 # Remove Task Function
+
 def remove_task():
-    task_to_remove = input("Enter the task you would like to remove: ")
+    task_to_remove = input("\nEnter the task you would like to remove:\n ")
     if task_to_remove in tasks:
         tasks.remove(task_to_remove)
-        print(f"Task {task_to_remove} has been removed successfully!")
+        print(f"\nTask {task_to_remove} has been removed successfully!\n")
     else:
-        print(f"Task {task_to_remove} is not in the list")
+        print(f"\nTask {task_to_remove} is not in the list\n")
 
 
 #Start programn - main function
 def main():
     while True:
         display_menu()
-        choice = input("Please select an option: ")
+        choice = input("\nPlease select an option:\n ")
         if choice == "1":
             add_task()
         elif choice == "2":
             view_tasks()
-        elif choice == "":
+        elif choice == "3":
             remove_task()
         elif choice == "4":
-            print("Thank you for using the To-Do List Application")
+            print("\nThank you for using the To-Do List Application\n")
             break
         else:
-            print("Invalid option. Please try again.")
+            print("\nInvalid option. Please try again.\n")
 
 main()
 
